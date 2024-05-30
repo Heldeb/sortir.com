c3e79f7d86b04c33f093151cb5746a2af330fa7e
Lynda IHADADENE <lynda.oumaouche@hotmail.fr>
2024-05-30 14:16:07 +0200

    gestion de la connexion utilisateurs

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,21 +7,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-
-
-class LoginController extends AbstractController
+class SecurityController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function connexion(AuthenticationUtils $authenticationUtils): Response
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('login/connexion.html.twig',['last_username'=>$lastUsername,'error'=>$error]);
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout():void
+    public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
